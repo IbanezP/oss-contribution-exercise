3b6ee1bc0d89c9599f035f087bf1eb6a5f199268
Pablo IBANEZ <ibanez.p.espci@gmail.com>
2016-05-25 17:18:16 +0200

    added useless comment

--- test_preprocess.py
+++ test_preprocess.py
@@ -3,6 +3,7 @@ from numpy.testing.utils import assert_almost_equal
 
 import preprocess
 
+# The followingis a list of tests for the print prout
 
 def test_center():
     # After centering, mean should be the desired mean and the
